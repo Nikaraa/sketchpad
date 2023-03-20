d5a406fe2a07024cc19a5fdea1af08d155c1ef91
nikaraa <giuseppemalara00001@gmail.com>
2023-03-20 17:40:45 +0100

    Add size slider

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const initialColor = 'rgb(63, 62, 62)'
-const initialMode = 'draw';
+const initialMode = 'draw'
+const initialSize = 16
 
+
+const sizeValue = document.querySelector('.size-value')
+
+const size = document.querySelector('.size')
 const grid = document.querySelector('.grid-container')
 const colorPicker = document.querySelector('.change-color')
 const eraser = document.querySelector('.eraser')
@@ -8,31 +13,48 @@ const clear = document.querySelector('.clear')
 const draw = document.querySelector('.draw')
 let currentColor = initialColor
 let currentMode = initialMode
+let currentSize = initialSize
 
 draw.onclick = () => setMode("draw")
 eraser.onclick = () => setMode("eraser")
 clear.onclick = () => reload()
 
+size.onmousemove = (e) => updateDiv(e.target.value)
+size.onchange = (e) => updateSize(e.target.value)
 colorPicker.oninput = (e) => setColor(e.target.value)
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
+function setSize(newSize) {
+    currentSize = newSize
+}
 
+function updateSize(value) {
+    setSize(value)
+    reload()
+}
 
-function createGrid() {
-    for (let i = 0; i < 16*16; i++) {
+function updateDiv(value) {
+    sizeValue.textContent = `${value}x${value}`
+}
+
+function createGrid(gridSize) {
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const gridElement = document.createElement('div')
         gridElement.classList.add('.grid-element')
         gridElement.addEventListener('mouseover', changeColor)
         gridElement.addEventListener('mousedown', changeColor)
         grid.appendChild(gridElement)
+        sizeValue.textContent=gridSize+"x"+gridSize
     }
 }
 
 function changeColor(e) {
-    if(!mouseDown&& e.type==="mouseover") return;
+    if (!mouseDown && e.type === "mouseover") return;
     if (currentMode === "draw") {
         e.target.style.backgroundColor = currentColor
     }
@@ -51,20 +73,20 @@ function setColor(newColor) {
 }
 
 function clearGrid() {
-    grid.innerHTML=""
+    grid.innerHTML = ""
 }
 
-function reload(){
+function reload() {
     clearGrid()
-    createGrid()
+    createGrid(currentSize)
 }
 
 function activeButt(newMode) {
-    if(newMode==="draw"){
+    if (newMode === "draw") {
         draw.classList.add('active')
         eraser.classList.remove('active')
     }
-    else if(newMode==="eraser"){
+    else if (newMode === "eraser") {
         eraser.classList.add('active')
         draw.classList.remove('active')
     }
@@ -72,7 +94,7 @@ function activeButt(newMode) {
 }
 
 window.onload = () => {
-    createGrid()
+    createGrid(initialSize)
     activeButt(initialMode)
 }
 
